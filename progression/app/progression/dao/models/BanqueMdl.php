c447618b3ee40c47b6259cbd4c78c79b7d56ce8d
Mahfuzur Ali <mali@dept-info.crosemont.quebec>
2024-02-21 20:25:06 +0000

    Création du modèle de banque

---
+++ progression/app/progression/dao/models/BanqueMdl.php
@@ -0,0 +1,46 @@
+<?php
+/*
+   This file is part of Progression.
+
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace progression\dao\models;
+
+// use progression\domaine\entité\banque\Url;
+use Illuminate\Database\Eloquent\Model;
+
+class BanqueMdl extends Model
+{
+	protected $table = "banque";
+	public $timestamps = false;
+	
+	protected $guarded = []
+/***	
+	protected $appends = ["url"];
+	
+	public function getUrlAttribute(): Url
+	{
+		$urls = array_column(Url::cases(), "value");
+		return Url::from($urls[$this->attributes["url"]]);
+	}
+	public function setUrlAttribute(Url $url): void
+	{
+		$urls = array_column(Url::cases(), "value");
+		$this->attributes["url"] = array_search($url->value, $urls);
+	}
+***/	
+}
+
+?>
