560a24fbc16c43e8038b9ff85a758ddcd9bbcfae
Alexandre Baudouin Vegas <abaudouinvegas@dept-info.crosemont.quebec>
2024-02-27 20:48:34 +0000

    fixed bug

--- progression/app/progression/dao/question/ChargeurQuestionGIT.php
+++ progression/app/progression/dao/question/ChargeurQuestionGIT.php
@@ -64,7 +64,8 @@ class ChargeurQuestionGIT extends Chargeur
     $liste_info_yml = null;
     $code_de_retour = null;
 
-    // Récupérer le chemin complet du fichier info.yml dans le dépôt cloné    exec("find $dossier_temporaire -name 'info.yml'", $liste_info_yml, $code_de_retour);
+    // Récupérer le chemin complet du fichier info.yml dans le dépôt cloné   
+    exec("find $dossier_temporaire -name 'info.yml'", $liste_info_yml, $code_de_retour);
     $chemin_fichier_dans_depot = $liste_info_yml[0];
     Log::debug("chemin du depot" . $chemin_fichier_dans_depot);
 
