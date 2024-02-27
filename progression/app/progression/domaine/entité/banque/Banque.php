eb35042ae01b46e3f9e361add724088593ba5ec0
Mahfuzur Ali <mali@dept-info.crosemont.quebec>
2024-02-27 20:51:47 +0000

    Création de l'entité Banque

---
+++ progression/app/progression/domaine/entité/banque/Banque.php
@@ -0,0 +1,37 @@
+<?php
+/*
+	This file is part of Progression.
+
+	Progression is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Progression is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace progression\domaine\entité\banque;
+
+use InvalidArgumentException;
+
+class Banque
+{
+	public int $id;
+	public string $nom;
+	public string $url;
+	public int $user_id;
+	
+	public function __construct($id, $nom, $url, $user_id)
+	{
+		$this->id = $id;
+		$this->nom = $nom;
+		$this->url = $url;
+		$this->user_id = $user_id
+	}
+}
