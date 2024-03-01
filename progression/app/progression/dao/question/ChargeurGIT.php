6342426b59c0592ff8977813f20fb43143e79cbc
Alexandre Baudouin Vegas <abaudouinvegas@dept-info.crosemont.quebec>
2024-03-01 15:49:23 +0000

    Update ChargeurGIT.php

--- progression/app/progression/dao/question/ChargeurGIT.php
+++ progression/app/progression/dao/question/ChargeurGIT.php
@@ -59,7 +59,7 @@ class ChargeurGIT extends Chargeur
 
 		exec("find $dossier_temporaire -name 'info.yml'", $liste_info_yml, $code_de_retour);
 
-		if (!$liste_info_yml) {
+		if ($code_de_retour !== 0 || !$liste_info_yml) {
 			throw new RunTimeException("Fichier info.yml inexistant");
 		}
 		
