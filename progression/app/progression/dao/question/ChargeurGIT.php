372a0f53791f9cb2a49442329c92189ec0c57238
Djemaoune Aimen <adjemaoune@ordralphabetix>
2024-03-17 23:42:53 -0400

    Correction d'un bugg concernant le message d'erreur (Exception)

--- progression/app/progression/dao/question/ChargeurGIT.php
+++ progression/app/progression/dao/question/ChargeurGIT.php
@@ -16,61 +16,60 @@
    along with Progression.  If not, see <https://www.gnu.org/licenses/>.
  */
 
- namespace progression\dao\question;
+namespace progression\dao\question;
 
- use Gitonomy\Git\Admin;
- use RuntimeException;
- use Illuminate\Support\Facades\Log;
- 
- class ChargeurGIT extends Chargeur
- {
-	 public function cloner_dépôt(string $url_du_dépôt): string
-	 {
-		 $dossier_memoir = "/tmp/memoire";
-		 $dossier_temporaire = $dossier_memoir . "/git_repo_" . uniqid();
-		 
-		 if (!is_dir($dossier_memoir)) {
-			 mkdir($dossier_memoir, 0777, true);
-			 Log::debug("Création du dossier mémoire : $dossier_memoir");
-		 }
-       
-		 Log::debug("Chemin du dépôt temporaire: " . $dossier_temporaire);
-		 Log::debug("URL du dépôt git: " . $url_du_dépôt);
- 
-		 try {
-			 Admin::cloneTo($dossier_temporaire, $url_du_dépôt, false);
-			 Log::debug("Dépôt cloné avec succès à : $dossier_temporaire");
-		 } catch (\ChargeurException $e) {
-			 Log::error("Erreur lors du clonage du dépôt : " . $e->getMessage());
-			 throw new ChargeurException(
-				 "Le clonage du dépôt git a échoué! Ce dépôt est peut-être privé ou n'existe pas.",
-			 );
-		 }
-		
- 
-		 return $dossier_temporaire;
-	 }
- 
-	 public function chercher_info(string $dossier_temporaire): string
-	 {
-		 $cheminRecherche = $dossier_temporaire . '/**/info.yml';
-		 $fichiers = glob($cheminRecherche, GLOB_BRACE);
-	 
-		 if (empty($fichiers)) {
-			 throw new RuntimeException("Fichier info.yml inexistant dans le dépôt.");
-		 }
-	 
-		 $chemin_fichier_dans_dépôt = $fichiers[0];
-		 Log::debug("Fichier info.yml trouvé : " . $chemin_fichier_dans_dépôt);
-	 
-		 return $chemin_fichier_dans_dépôt;
-	 }
- 
-	 public function supprimer_dossier_temporaire(string $dossier_temporaire): void
-	 {
-		 if (is_dir($dossier_temporaire)) {
-			 system("rm -rf " . escapeshellarg($dossier_temporaire));
-			 Log::debug("Dossier temporaire supprimé : $dossier_temporaire");
-		 }
-	 }
- }
+use Gitonomy\Git\Admin;
+use RuntimeException;
+use Illuminate\Support\Facades\Log;
+
+class ChargeurGIT extends Chargeur
+{
+	public function cloner_dépôt(string $url_du_dépôt): string
+	{
+		$dossier_memoir = "/tmp/memoire";
+		$dossier_temporaire = $dossier_memoir . "/git_repo_" . uniqid();
+
+		if (!is_dir($dossier_memoir)) {
+			mkdir($dossier_memoir, 0777, true);
+			Log::debug("Création du dossier mémoire : $dossier_memoir");
+		}
+
+		Log::debug("Chemin du dépôt temporaire: " . $dossier_temporaire);
+		Log::debug("URL du dépôt git: " . $url_du_dépôt);
+
+		try {
+			Admin::cloneTo($dossier_temporaire, $url_du_dépôt, false);
+			Log::debug("Dépôt cloné avec succès à : $dossier_temporaire");
+		} catch (ChargeurException $e) {
+			Log::error("Erreur lors du clonage du dépôt : " . $e->getMessage());
+			throw new ChargeurException(
+				"Le clonage du dépôt git a échoué! Ce dépôt est peut-être privé ou n'existe pas.",
+			);
+		}
+
+		return $dossier_temporaire;
+	}
+
+	public function chercher_info(string $dossier_temporaire): string
+	{
+		$cheminRecherche = $dossier_temporaire . "/**/info.yml";
+		$fichiers = glob($cheminRecherche, GLOB_BRACE);
+
+		if (empty($fichiers)) {
+			throw new RuntimeException("Fichier info.yml inexistant dans le dépôt.");
+		}
+
+		$chemin_fichier_dans_dépôt = $fichiers[0];
+		Log::debug("Fichier info.yml trouvé : " . $chemin_fichier_dans_dépôt);
+
+		return $chemin_fichier_dans_dépôt;
+	}
+
+	public function supprimer_dossier_temporaire(string $dossier_temporaire): void
+	{
+		if (is_dir($dossier_temporaire)) {
+			system("rm -rf " . escapeshellarg($dossier_temporaire));
+			Log::debug("Dossier temporaire supprimé : $dossier_temporaire");
+		}
+	}
+}
